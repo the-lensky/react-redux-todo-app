345cb88e458c4c00fd9f1b20b96b9895e8e52fde
ravesnature <76727818+ravesnature@users.noreply.github.com>
2021-02-20 11:49:18 +0300

    -add redux env

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import './App.css'
+import React from 'react'
+import {Provider} from 'react-redux'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Header from './components/header/Header'
+import Main from './components/main/Main'
+import About from './components/about/About'
+import store from './store'
 
-function App() {
+
+const App = () => {
     return (
-       <h1></h1>
+        <Provider store={store}>
+            <Router>
+                <Header/>
+                <div className='container'>
+                    <Switch>
+                        <Route exact path='/' component={Main}/>
+                        <Route exact path='/about' component={About}/>
+                    </Switch>
+                </div>
+            </Router>
+        </Provider>
     )
 }
 
---
+++ src/reducers/todosReducer.js
@@ -0,0 +1,59 @@
+import {
+    ADD_TODO,
+    TODOS_LOADED,
+    DELETE_TODO,
+    MARK_COMPLETED,
+} from '../actions/types'
+
+const initialState = {
+    todos: [],
+}
+
+const todosReducer = (state = initialState, action) => {
+    const {type, payload} = action
+    switch (type) {
+        case TODOS_LOADED:
+            const todoItems = JSON.parse(localStorage.getItem('todos'))
+            return {
+                ...state,
+                todos: !todoItems ? [] : todoItems,
+            }
+        case ADD_TODO:
+            if (!localStorage.todos) {
+                let todos = []
+                todos.push(payload)
+                localStorage.setItem('todos', JSON.stringify(todos))
+            } else {
+                let todos = JSON.parse(localStorage.getItem('todos'))
+                todos.unshift(payload)
+                localStorage.setItem('todos', JSON.stringify(todos))
+            }
+            return {
+                ...state,
+                todos: [payload, ...state.todos],
+            }
+        case DELETE_TODO:
+            let todos = JSON.parse(localStorage.getItem('todos'))
+            todos.forEach(todo => todo.id === payload && todos.splice(todo, 1))
+            localStorage.setItem('todos', JSON.stringify(todos))
+            return {
+                ...state,
+                todos: state.todos.filter(todo => todo.id !== payload),
+            }
+        case MARK_COMPLETED:
+            let todoitems = JSON.parse(localStorage.getItem('todos'))
+            todoitems.map(todo =>
+                todo.id === payload.id ? (todo.isCompleted = !todo.isCompleted) : todo
+            )
+            localStorage.setItem('todos', JSON.stringify(todoitems))
+            return {
+                ...state,
+                todos: state.todos.map(todo =>
+                    todo.id === payload.id ? payload : todo
+                ),
+            }
+        default:
+            return state
+    }
+}
+export default todosReducer
